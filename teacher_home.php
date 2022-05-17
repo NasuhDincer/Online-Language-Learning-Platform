0ae6c64792fc92e364d82b83938c90e77265007a
Mustafa Yusa Babademez <63500947+Mustafa-Yusa-Babademez@users.noreply.github.com>
2022-05-17 21:59:56 +0300

    Update teacher_home.php

--- teacher_home.php
+++ teacher_home.php
@@ -4,8 +4,14 @@ include "configure.php";
 // teaches is a table that has foreign key from teacher and foreign key from class 
 // get classes that teacher teaches
 session_start();
+echo "<br><br>";
+echo "<a href='teacher_home.php'>Home</a>   ";
+echo "<a href='teacher_class.php'>Class</a>  ";
+echo "<a href='teacher_home.php'>Profile</a>    ";
+echo "  <a href='user.html'>Logout</a>";
 
-echo "Welcome " . $_SESSION["user_name"];
+
+echo "<br>Welcome " . $_SESSION["user_name"];
 echo "<br><br><br>";
 
 $teacher_id = $_SESSION["userid"];
@@ -40,6 +46,7 @@ if ($result3->num_rows > 0) {
     }
 }
 
+
 // get all activities of the classes that teacher teaches
 $sql3 = "SELECT * FROM class NATURAL JOIN teaches NATURAL JOIN activity NATURAL JOIN with_in WHERE teacher_id = '$teacher_id'";
 $result4 = $conn->query($sql3);
@@ -52,17 +59,28 @@ if ($result4->num_rows > 0) {
     }
 }
 
-//print class name and student number in each class
-for ($i = 0; $i < count($class_name); $i++) {
-    echo $class_name[$i] . ": " . $class_count[$i] . "<br>";
-}
 
-//print activity name
-for ($i = 0; $i < count($activity_name); $i++) {
-    echo $activity_name[$i] . "<br>";
-}
 
 
 
 ?>
+
+ </style>
+ <div class="signup-form">
+     <form action="" method="post">
+         <h2>Classes</h2>
+         <?php
+         for ($i = 0; $i < count($class_name); $i++) {
+             echo "<h3>Class name: ".$class_name[$i]."</h3>";
+             echo "<h4>Student count: ".$class_count[$i]."</h4>";
+         }
+         ?>
+         <h2>Activities</h2>
+         <?php
+         for ($i = 0; $i < count($activity_name); $i++) {
+             echo "<h3>Activity name: ".$activity_name[$i]."</h3>";
+         }
+         ?>
+     </form>
+ </div>
 
